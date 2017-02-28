82b9de9f14f48c2d02397d443bc49e3e577b916d
Unknown <andrewpetersell@gmail.com>
2017-02-27 22:24:04 -0500

    Copied Game from Week 1 w/ Some Changes

---
+++ Week 5 HW/Assets/Scripts/StageBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class StageBuilder : MonoBehaviour {
+
+	public GameObject playerGuy; 
+	public GameObject playerGal; 
+	public GameObject block; 
+	public string [] fileNames;
+	public static int stageNum; 
+
+
+	// Use this for initialization
+	void Start () {
+		string fileName = fileNames [stageNum];
+		string filePath = Application.dataPath + "/" + fileName; 
+
+		StreamReader sr = new StreamReader (filePath);
+
+		int yPos = 0;
+
+		while(!sr.EndOfStream)
+		{
+			string line = sr.ReadLine(); 
+
+			for(int xPos = 0; xPos < line.Length; xPos++) 
+			{
+
+				if(line[xPos] == 'X')
+				{ 
+
+				}
+
+				if (line [xPos] == 'B') 
+				{
+					 
+				}
+			}
+			yPos--; 
+		}
+
+		sr.Close();
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
